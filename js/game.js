49d56bf89bdaad95740c2cfac534d5ce6641b1b2
aurelie-castro <acdemarly@gmail.com>
2021-07-01 18:50:08 +0200

    finishing touches

--- js/game.js
+++ js/game.js
@@ -33,9 +33,6 @@ var wrongSound;
 var correctSound;
 var finishSound;
 
-var soundButton;
-var hasBeenClicked;
-
 //var du background
 var gameBg;
 
@@ -68,9 +65,6 @@ function preload() {
     this.load.audio('correct', './assets/correct.wav');
     this.load.audio('finish', './assets/congratulations.wav');
     
-    //---sound button----
-    this.load.image('soundBtn', './assets/volume-up.png');
-    
     //---star at the end---
     this.load.image('star', './assets/b-badge.png');
     
@@ -99,14 +93,6 @@ function create() {
     wrongSound = this.sound.add('wrong');
     correctSound = this.sound.add('correct');
     finishSound = this.sound.add('finish');
-    
-    //----audio  btn----
-    soundButton = this.add.image(50,50, 'soundBtn');
-    soundButton.setScale(0.1);
-    soundButton.setInteractive();
-    soundButton.alpha = 0.5;
-    soundButton.on('pointerdown', enableMusic);
-
     
     //drop off counter
     successfulDropoff = 0;
@@ -222,7 +208,6 @@ else{
         }
         
         if(successfulDropoff === 6){
-            console.log("well done!!!!");
             nextArrow.setVisible(true);
             nextArrow.setInteractive();
             finishSound.play();
@@ -248,18 +233,9 @@ function update() {
 
     }
     
-        if (hasBeenClicked === true){
-            soundButton.alpha = 1;
-        }
-    
 }
 
 function onClick(){
     window.location.replace("https://games.caramel.be/pinocchio/index.html");
 
 }
-
-function enableMusic(){
-    hasBeenClicked = true;
-}
-
